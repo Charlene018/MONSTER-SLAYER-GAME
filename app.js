0838dfe36a4a973ade2b6b35e30814f09771f7c4
Charlene Bendaña <bendanacharlene@gmail.com>
2021-12-07 22:43:19 -0800

    Added methods for special attacks of player and monster and healing for players

--- app.js
+++ app.js
@@ -45,4 +45,54 @@ function getRandomValue(min , max){
             this.winner = 'player';
           }
         }
-      }, 
+      },  methods: {
+        newGame() {
+          this.playerHealth = 100;
+          this.monsterHealth = 100;
+          this.currentRound = 0;
+          this.winner = null;
+          this.logMessages = [];
+        },
+        attackMonster() {
+          this.currentRound ++
+          const attackValue = getRandomValue (8, 13);
+          this.monsterHealth -= attackValue;
+          this.existingMessage('player', 'attack', attackValue);
+          this.attackPlayer()
+        },
+        attackPlayer () {
+          const attackValue =getRandomValue (10, 17);
+          this.playerHealth -= attackValue;
+          this.existingMessage('monster', 'attack', attackValue);
+        },
+        specialAttackMonster(){
+          this.currentRound ++
+          const attackValue = getRandomValue (12, 23);
+          this.monsterHealth -= attackValue;
+          this.existingMessage('player', 'special-attack', attackValue);
+          this.attackPlayer()
+        },
+        healPlayer() {
+          this.currentRound ++;
+          const healValue = getRandomValue(14, 22);
+          if(this.playerHealth + healValue > 100 ) {
+            this.playerHealth = 100;
+          }
+          else{
+          this.playerHealth += healValue;
+          }
+          this.existingMessage('player', 'heal', healValue);
+          this.attackPlayer()
+        },
+        playerSurrender() {
+          this.winner = 'monster';
+        },
+        existingMessage(who, what, value) {
+          this.logMessages.unshift({
+            actionBy: who,
+            actionType: what,
+            actionValue: value
+          });
+        }
+      }
+    })
